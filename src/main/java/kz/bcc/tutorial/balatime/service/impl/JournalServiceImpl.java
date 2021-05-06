21d625e85fa7d7704ad92d98057b68a5fc20d2ec
TEMIRLAN <temirlanturikanov@gmail.com>
2021-05-06 11:06:38 +0600

    add services(impl) Journal, Role

---
+++ src/main/java/kz/bcc/tutorial/balatime/service/impl/JournalServiceImpl.java
@@ -0,0 +1,42 @@
+package kz.bcc.tutorial.balatime.service.impl;
+
+import kz.bcc.tutorial.balatime.model.Journal;
+import kz.bcc.tutorial.balatime.repository.JournalRepository;
+import kz.bcc.tutorial.balatime.service.JournalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class JournalServiceImpl implements JournalService {
+    @Autowired
+    public JournalRepository journalRepository;
+
+
+    @Override
+    public Journal create(Journal journal) {
+        return journalRepository.save(journal);
+    }
+
+    @Override
+    public Journal getById(Integer id) {
+        Optional<Journal> optionalJournal = journalRepository.findById(id);
+        if (optionalJournal.isPresent()) {
+            return journalRepository.findById(id).get();
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public List<Journal> getAll() {
+        return journalRepository.findAll();
+    }
+
+    @Override
+    public void delete(Integer id) {
+
+    }
+}
