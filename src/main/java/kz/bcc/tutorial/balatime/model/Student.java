d321fe0ff80804895215f6c268bca06f40971ae3
TEMIRLAN <temirlanturikanov@gmail.com>
2021-06-07 08:18:31 +0600

    add FK classId to studentModel

--- src/main/java/kz/bcc/tutorial/balatime/model/Student.java
+++ src/main/java/kz/bcc/tutorial/balatime/model/Student.java
@@ -28,6 +28,9 @@ public class Student {
     private String firstName;
     @Column(name = "last_name")
     private String lastName;
+    @ManyToOne
+    @JoinColumn(name = "class_id", insertable = false, updatable = false)
+    private Classs classs;
     @Column(name = "class_id")
     private Integer classId;
     @Column(name = "birth_date")
